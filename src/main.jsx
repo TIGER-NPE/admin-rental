d9cbdce01bf44e1fd06e2574abb2087ec9127b54
unknown <patiencetiger2@gmail.com>
2026-02-06 02:16:17 +0200

    Fix API URL to use car-rental-api-pp6g.onrender.com

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { LanguageProvider } from './context/LanguageContext.jsx'
+import CarsPage from './pages/CarsPage.jsx'
+import TermsPage from './pages/TermsPage.jsx'
+import DriversPage from './pages/DriversPage.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
+      <LanguageProvider>
+        <Routes>
+          <Route path="/" element={<CarsPage />} />
+          <Route path="/terms" element={<TermsPage />} />
+          <Route path="/drivers" element={<DriversPage />} />
+        </Routes>
+      </LanguageProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
